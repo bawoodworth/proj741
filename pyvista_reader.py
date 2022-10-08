6d8e4536acf49b186ec46265cac0204cb32736d8
bawoodworth <bwoodworth121@gmail.com>
2022-10-08 18:21:50 -0400

    fix script to work with updated paths

--- pyvista_reader.py
+++ pyvista_reader.py
@@ -1,30 +1,40 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Oct  8 01:50:37 2022
-
-@author: Brian Woodworrth
-"""
-
 import numpy as np
 import pyvista
 
-filename = 'test.vtp'
 
-reader = pyvista.get_reader(filename)
-
-print(f"Number of point arrays: {reader.number_point_arrays}")
-print(f"Available point data:   {reader.point_array_names}")
-print(f"Number of cell arrays:  {reader.number_cell_arrays}")
-print(f"Available cell data:    {reader.cell_array_names}")
-
-
-mesh = reader.read()
-
-print(f"Read arrays:        {mesh.array_names}")
+def get_mesh(filename):
+    reader = pyvista.get_reader(filename)
+    # print(f"Number of point arrays: {reader.number_point_arrays}")
+    # print(f"Available point data:   {reader.point_array_names}")
+    # print(f"Number of cell arrays:  {reader.number_cell_arrays}")
+    # print(f"Available cell data:    {reader.cell_array_names}")
+    mesh = reader.read()
+    # print(f"Read arrays:        {mesh.array_names}")
+    return mesh
 
 
-plotter = pyvista.Plotter()
+def plot_mesh(mesh):
+    plotter = pyvista.Plotter()
+    bar_args = {
+        'title': "Temperate \N{DEGREE SIGN}C",
+        'height': 0.25,
+        'vertical': True,
+        'position_x': 0.05,
+        'position_y': 0.05,
+        'title_font_size':20,
+        'label_font_size':14,
+        'n_labels':3
+        }
+    plotter.add_mesh(mesh, scalars='T', cmap="bmy", lighting=True, scalar_bar_args=bar_args)
+    plotter.show()
+    
 
-# plotter.subplot(0)
-plotter.add_mesh(mesh, scalars='T', show_scalar_bar=True)
-plotter.show()
+def main():
+    filename = './data/test.vtp'
+    mesh = get_mesh(filename)
+    plot_mesh(mesh)
+
+
+if __name__ == '__main__':
+    main()
