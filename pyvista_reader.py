0798a01891c765dea9927a0947037dca216c1235
bawoodworth <bwoodworth121@gmail.com>
2022-10-17 00:09:52 -0400

    added some better data models, updated script to animate

--- pyvista_reader.py
+++ pyvista_reader.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pyvista
+import glob
+import os
+import time
+import pyvistaqt
 
 
 def get_mesh(filename):
@@ -14,8 +18,7 @@ def get_mesh(filename):
     return mesh
 
 
-def plot_mesh(mesh):
-    plotter = pyvista.Plotter()
+def plot_mesh(plotter, mesh):
     bar_args = {
         'title': "Temperate \N{DEGREE SIGN}C",
         'height': 0.25,
@@ -24,17 +27,28 @@ def plot_mesh(mesh):
         'position_y': 0.05,
         'title_font_size':20,
         'label_font_size':14,
-        'n_labels':3
+        'n_labels':3,
+        'use_opacity':True,
         }
-    plotter.add_mesh(mesh, scalars='T', cmap="bmy", lighting=True, scalar_bar_args=bar_args)
-    plotter.show()
+    plotter.add_mesh(mesh, clim=[273,350], cmap="jet", lighting=True, scalar_bar_args=bar_args)
+    plotter.camera.view_angle = 30
+    plotter.view_vector([-.3,-.5,.2])
+    return plotter
     
 
 def main():
-    filename = './data/test.vtp'
-    mesh = get_mesh(filename)
-    plot_mesh(mesh)
+    data_dir = './data'
+    data_files = glob.glob(os.path.join(data_dir, '*.vtp'))
+    plotter = pyvistaqt.BackgroundPlotter()
+    plotter.show()
+    for data_file in data_files:
+        print(data_file)
+        mesh = get_mesh(data_file)
+        plotter = plot_mesh(plotter, mesh)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
     main()
+    
+    
